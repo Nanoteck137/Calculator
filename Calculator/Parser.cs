c4e2f7b070406050df03141c5b91294b5b18c583
Nanoteck137 <patrik.millvik@gmail.com>
2019-10-30 12:52:27 +0100

    Började på parsern

---
+++ Calculator/Parser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    abstract class Node { }
+
+    class NumberNode : Node
+    {
+        public ulong Value { get; private set; }
+
+        public NumberNode(ulong value)
+        {
+            this.Value = value;
+        }
+    }
+
+    class BinaryOpNode : Node
+    {
+        public Node Left { get; private set; }
+        public Node Right { get; private set; }
+        public TokenType Op { get; private set; }
+
+        public BinaryOpNode(Node left, Node right, TokenType op)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Op = op;
+        }
+    }
+
+    class Parser
+    {
+        private Lexer lexer;
+
+        public Parser(Lexer lexer)
+        {
+            this.lexer = lexer;
+        }
+
+        public Node ParseOperand()
+        {
+            return null;
+        }
+
+        public Node ParseMul()
+        {
+            return null;
+        }
+
+        public Node ParseAdd()
+        {
+            return null;
+        }
+
+        public Node Parse()
+        {
+            return ParseAdd();
+        }
+    }
+}
